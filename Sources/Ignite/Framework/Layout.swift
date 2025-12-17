f942d5e296b2bcdc7953fff9890e93a7c1f39f3f
Morten Bek Ditlevsen <morten@ka-ching.dk>
2025-12-17 14:43:14 +0100

    Skip root Section for all pages

--- Sources/Ignite/Framework/Layout.swift
+++ Sources/Ignite/Framework/Layout.swift
@@ -30,7 +30,36 @@ public protocol Layout {
 public extension Layout {
     /// The current page being rendered.
     var content: some HTML {
-        Section(PublishingContext.shared.environment.pageContent)
-            .class("ig-main-content")
+        EmptySection(PublishingContext.shared.environment.pageContent)
+    }
+}
+
+public struct EmptySection: HTML, FormItem {
+    /// The content and behavior of this HTML.
+    public var body: some HTML { self }
+    
+    /// The standard set of control attributes for HTML elements.
+    public var attributes = CoreAttributes()
+    
+    /// Whether this HTML belongs to the framework.
+    public var isPrimitive: Bool { true }
+   
+    var content: any BodyElement
+    
+    init(_ content: any BodyElement) {
+        self.content = content
+    }
+    
+    /// Creates a section that renders as a `div` element.
+    /// - Parameter content: The content to display within this section.
+    public init(
+        @HTMLBuilder content: () -> some BodyElement
+    ) {
+        self.content = content()
+    }
+    
+    public func markup() -> Markup {
+        let contentHTML = content.markupString()
+        return Markup(contentHTML)
     }
 }
